ceba119b6eca868356db662f4240d58d119d6e87
anyamri <ayamriska@yahoo.com>
2022-12-28 11:02:43 -0600

    Final

--- tennis_ace.py
+++ tennis_ace.py
@@ -44,26 +44,44 @@ plt.show()
 print(model.score(features_2_test,outcome_2_test))
 
 ## perform two feature linear regressions here:
+features_3 = df[['breakpointsopportunities','firstservereturnpointswon']]
+outcome_3 = df[['winnings']]
 
+features_3_train, features_3_test, outcome_3_train, outcome_3_test = train_test_split(features_3, outcome_3, train_size = 0.8)
+model.fit(features_3_train,outcome_3_train)
+model_3 = model.score(features_3_test,outcome_3_test)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+prediction_3 = model.predict(features_3_test)
+plt.scatter(outcome_3_test,prediction_3, alpha=0.4)
+plt.title('3')
+plt.show()
+plt.clf()
+print('Model 3 results:', model_3)
 
 ## perform multiple feature linear regressions here:
+features_4 = df[['breakpointsfaced', 'breakpointsopportunities', 'totalpointswon', 'wins', 'ranking', 'totalpointswon']]
+outcome_4 = df[['winnings']]
+features_4_train, features_4_test, outcome_4_train, outcome_4_test = train_test_split(features_4, outcome_4, train_size=0.8)
+model.fit(features_4_train,outcome_4_train)
+model_4 = model.score(features_4_test, outcome_4_test)
+
+prediction_4 = model.predict(features_4_test)
+plt.scatter(outcome_4_test, prediction_4,alpha=0.4)
+plt.title('4')
+plt.show()
+print('Model 4 results:', model_4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
