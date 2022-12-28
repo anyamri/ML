3bece5907eac88b0293018accb9c9af1204001ee
anyamri <ayamriska@yahoo.com>
2022-12-28 10:36:21 -0600

    On branch main

--- tennis_ace.py
+++ tennis_ace.py
@@ -29,24 +29,19 @@ prediction = model.predict(features_test)
 plt.scatter(outcome_test,prediction,alpha=0.4)
 plt.title('breakpointsfaced vs wins')
 plt.show()
+plt.clf()
 
+features_2 = df[['breakpointsopportunities']]
+outcome_2 = df[['wins']]
+features_2_train, features_2_test, outcome_2_train, outcome_2_test = train_test_split(features_2,outcome_2,train_size=0.8)
+model.fit(features_2_train, outcome_2_train)
+model.score(features_2_test,outcome_2_test)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+prediction_2 = model.predict(features_2_test)
+plt.scatter(outcome_2_test,prediction_2, alpha=0.4)
+plt.title('breakpointsopporunities vs wins')
+plt.show()
+print(model.score(features_2_test,outcome_2_test))
 
 ## perform two feature linear regressions here:
 
